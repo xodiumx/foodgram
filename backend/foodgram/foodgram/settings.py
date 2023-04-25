d7a101fd128f9c2446529fcc4dd17b9c026b91c4
Maksim Alekseev <am.xlexeev@gmail.com>
2023-04-25 10:27:56 +0500

    fix permissions

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'fsgdadsfae#^F#&RFG$&#FG@daasd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Allows Ips
 
